a1c2291d59a6efb75d23ecd032e18a723f554468
Fred Condo <fred@fredcondo.com>
2025-11-20 15:29:27 -0800

    Update QA tooling

--- rector.php
+++ rector.php
@@ -5,16 +5,15 @@ declare(strict_types=1);
 use Cambis\SilverstripeRector\Set\ValueObject\SilverstripeLevelSetList;
 use Cambis\SilverstripeRector\Set\ValueObject\SilverstripeSetList;
 use Rector\Config\RectorConfig;
-use Rector\Php80\Rector\Switch_\ChangeSwitchToMatchRector;
 
 return RectorConfig::configure()
     ->withPaths([
         __DIR__ . '/src',
         __DIR__ . '/tests/Src',
     ])
-    ->withPhpSets(php80: true)
+    ->withPhpSets(php83: true)
     ->withImportNames(importShortClasses: false)
     ->withSets([
-        SilverstripeLevelSetList::UP_TO_SILVERSTRIPE_50,
+        SilverstripeLevelSetList::UP_TO_SILVERSTRIPE_60,
         SilverstripeSetList::CODE_QUALITY,
     ]);
